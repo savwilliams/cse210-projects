8c09cee6e367592915701049a4a9533bd78ad0be
savwilliams <wil24021@byui.edu>
2025-09-23 15:54:11 -0600

    added notes to

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+
+        // to capitalize first letter of each word:
+        
+        //string randomWords = "hello sandbox word!!!";
+        //TextInfo = CultureInfo.CurrentCulture.TextInfo;
+        //string capitalized = TextInfo.ToTitleCase(randomWords);
+        //Console.WriteLine(capitalized);
+
+        //dont forget to put using.System.Globalization; at top of file
     }
 }
