3a3be5fd83a14a0db2b2c5ecfbdc9289cf8a5174
savwilliams <wil24021@byui.edu>
2025-09-23 15:52:23 -0600

    completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomNumberGenerator = new Random();
+        int magicNumber = randomNumberGenerator.Next(1, 100);
+
+        int userGuess = -1;
+              
+        while (magicNumber != userGuess)
+        {
+            Console.Write("What is your guess? ");
+
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+        }
+
+        if (userGuess == magicNumber)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        
     }
 }
