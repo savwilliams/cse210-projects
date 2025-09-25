fcc31b563dc7f898130c4ce1c20c1622273a6eff
savwilliams <wil24021@byui.edu>
2025-09-25 14:21:14 -0600

    Completed prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        int userNumberInput = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+        List<int> numberList = new List<int>();
+
+
+        while (userNumberInput != 0)
+        {
+
+            Console.Write("Enter number: ");
+
+            userNumberInput = int.Parse(Console.ReadLine());
+
+            if (userNumberInput != 0)
+            {
+                numberList.Add(userNumberInput);
+            }
+
+        }
+
+
+        int sum = numberList.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double avg = numberList.Average();
+        Console.WriteLine($"The average is: {Math.Round(avg,2)}");
+
+        int max_num = numberList.Max();
+        Console.WriteLine($"The largest number is: {max_num}");
+
     }
 }
