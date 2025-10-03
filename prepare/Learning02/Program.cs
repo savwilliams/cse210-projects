e83128157ed6f595541674e12311cc95fa7a79cc
savwilliams <wil24021@byui.edu>
2025-10-02 23:47:55 -0600

    Completed Learning 02 (resume) assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Web Designer";
+        job1._startYear = 2021;
+        job1._endYear = 2024;
+        //job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+        //job2.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume._name = "Ann Helms";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayJobDetails();
+
+
     }
 }
