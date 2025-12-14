16dc37369e364f8cfb38bfd9257ef06c649e02b2
savwilliams <wil24021@byui.edu>
2025-12-13 20:56:31 -0700

    Began working on Linq filtering logic in menu (added moodString). Re-arranged questionaire order to first ask for mood, then genre, then energy level

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -6,6 +6,7 @@ class Program
     {
         //===== Local variables ===== 
         string moodSelection;
+        string moodString = "" ;
 
         //===== Questionaire Dictionaries ===== 
         Dictionary<string, List<string>> allowedMoods = new Dictionary<string, List<string>>
@@ -25,29 +26,50 @@ class Program
         };
 
         //===== Questionaire Menu ===== 
-        Console.WriteLine("Welcome to your playlist generator! Complete a questionaire form and I'll curate a personalized playlist for you!");
+        Console.WriteLine("Welcome to your playlist generator! Complete a questionaire and I'll curate a personalized playlist for you!");
         Console.WriteLine("=== Press enter to begin ===");
         Console.ReadLine();
 
+            //-- Prompt for Mood --        
+        Console.WriteLine("=== What mood are you looking for? ===");
+        Console.WriteLine("(a) Happy");
+        Console.WriteLine("(b) Sad");
+        Console.WriteLine("(c) Chill");
+        Console.WriteLine("(d) Intense");        
+        moodSelection = Console.ReadLine();
+
+        if(moodSelection == "a")
+            moodString = "Happy";
+        else if(moodSelection == "b")
+            moodString = "Sad";
+        else if(moodSelection == "c")
+            moodString = "Chill";
+        else if(moodSelection == "d")
+            moodString = "Intense";
+
+
+        //List<string> moodOptions = 
+        // IEnumerable<string> query = allowedMoods[moodString].Where(word => word.Length == 3);
+
+        // foreach (string str in query)
+        // {
+        //     Console.WriteLine(str);
+        // }
+
+            //-- Prompt for Genre --
         Console.WriteLine("=== Which genre are you most interested in listening to? ===");
         Console.WriteLine("(a) Rock");
         Console.WriteLine("(b) Electronic Dance Music");
         Console.WriteLine("(c) Pop");
         Console.WriteLine("(d) Folk");
-        moodSelection = Console.ReadLine();
-
-        Console.WriteLine("=== What mood are you looking for? ===");
-        Console.WriteLine("(a) Happy");
-        Console.WriteLine("(b) Sad");
-        Console.WriteLine("(c) Chill");
-        Console.WriteLine("(d) Intense");
-        Console.ReadLine();
         
+
+            //-- Prompt for Energy Level --
         Console.WriteLine("=== What energy level do you want your playlist to have? ===");
         Console.WriteLine("(a) Low");
         Console.WriteLine("(b) Medium");
         Console.WriteLine("(c) High");
-        Console.WriteLine("(d) Folk");
+        Console.ReadLine();
         
     }
 }
